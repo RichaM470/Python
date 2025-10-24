f3fca4ad52147b9a88fd83e68737456f2e7123df
RichaM470 <richamendapara0208@gmail.com>
2025-10-24 11:55:18 +0100

    Add files via upload

---
+++ storing birthdays acp.py
@@ -0,0 +1,8 @@
+Richa=2
+mum=23
+dad=15
+sister=11
+print("Birthday date of Richa is", Richa)
+print("Birthday date of mum is", mum)
+print("Birthday date of dad is", dad)
+print("Birthday date of sister is", sister)

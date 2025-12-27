4ed90c6f7fb1bb0cbd3d6fe7d8b4e2eb86da3cea
RichaM470 <richamendapara0208@gmail.com>
2025-12-27 11:31:43 +0000

    Add files via upload

---
+++ Swapped values.py
@@ -0,0 +1,10 @@
+num1=5
+num2=7
+num3=9
+print("Original value of num1 is", num1)
+print("Original value of num2 is", num2)
+print("Original value of num3 is", num3)
+num1=num2
+num2=num3
+print("Swapped values of num2 is", num2)
+print("Swapped values of num3 is", num3)
